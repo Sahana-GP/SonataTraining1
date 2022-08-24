19d09b44bc0bcad536f58bf9dc41fdd3ff9d9cd8
Sahana <sahana.p@sonata-software.com>
2022-08-24 17:27:32 +0530

    cal

--- src/test/java/com/example/demo/CalTest.java
+++ src/test/java/com/example/demo/CalTest.java
@@ -1,9 +1,11 @@
-package com.sonata.two;
+package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+
+import com.sonata.two.Calculator;
 
 public class CalTest {
 	Calculator c1 = new Calculator();
