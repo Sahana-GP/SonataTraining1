f5b90b50d1f101b27bf93253c60ed7dd6fc975b2
Sahana <sahana.p@sonata-software.com>
2022-08-24 17:10:18 +0530

    commit

---
+++ src/main/java/com/sonata/two/Calculator.java
@@ -0,0 +1,25 @@
+package com.sonata.two;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
+	
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator();
+		System.out.println(c1.add(10, 20));
+		System.out.println(c1.mul(10, 20));
+		System.out.println(c1.div(10, 20));
+
+	}
+
+}
---
+++ src/test/java/com/example/demo/EmpTest.java
@@ -0,0 +1,99 @@
+/*<<<<<<< HEAD
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import com.sonata.Impl.EmpImpl;
+import com.sonata.Model.Employee;
+
+
+public class EmpTest {
+
+	
+	@Test
+	public void yearlySalTest1() {
+		Employee e1 = new Employee();
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(12000, e2.yearlySal(e1));
+	}
+	
+	@Test
+	public void yearlySalTest2() {
+		Employee e1 = new Employee(); 
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(12000, e2.yearlySal(e1));
+	}
+	
+	@Test
+	public void aSalTest1() {
+		Employee e1 = new Employee();
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(5000, e2.appSal(e1));
+	}
+	
+	@Test
+	public void aSalTest2() {
+		Employee e1 = new Employee();
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(5000, e2.appSal(e1));
+	}
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+}>>>>>>> 7a3bc15d96ff7c8c343cb78051091bff535c180f
+======= */
+package com.example.demo;
+
+//import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import com.sonata.Impl.EmpImpl;
+import com.sonata.Model.Employee;
+
+
+public class EmpTest {
+
+	
+	//@Test
+	public void yearlySalTest1() {
+		Employee e1 = new Employee();
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(12000, e2.yearlySal(e1));
+	}
+	
+	/*@Test
+	public void yearlySalTest2() {
+		Employee e1 = new Employee(); 
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(12000, e2.yearlySal(e1));
+	}
+	
+	@Test
+	public void aSalTest1() {
+		Employee e1 = new Employee();
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(5000, e2.appSal(e1));
+	}
+	
+	@Test
+	public void aSalTest2() {
+		Employee e1 = new Employee();
+		e1.setEmplSal(1000);
+		EmpImpl e2 = new EmpImpl();
+		Assertions.assertEquals(5000, e2.appSal(e1));
+	}
+	*/
+
+	
+
+}
+
